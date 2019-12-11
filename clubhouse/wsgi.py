c1e191d9cf57c5ab15edcabfe4d4e60fa1071a3a
Stephen Van Dahm <code@vandahm.com>
2019-12-10 19:56:29 -0600

    remove dotenv

--- clubhouse/wsgi.py
+++ clubhouse/wsgi.py
@@ -9,9 +9,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clubhouse.settings')
