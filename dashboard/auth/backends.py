dec863c669768c5d67d87ea98116a809e82c263d
Stephen Van Dahm <code@vandahm.com>
2020-05-14 10:38:18 -0500

    sigh

--- dashboard/auth/backends.py
+++ dashboard/auth/backends.py
@@ -3,7 +3,7 @@ from ..models import User
 
 class CognitoAuthenticationBackend(OIDCAuthenticationBackend):
     def filter_users_by_claims(self, claims):
-        email = claims.get('sub')
+        sub = claims.get('sub')
         if not sub:
             return self.UserModel.objects.none()
 
