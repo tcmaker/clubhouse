99501ae73e7ff87927585854599812519b28c677
Stephen Van Dahm <code@vandahm.com>
2020-05-18 19:09:22 -0500

    clean up admin form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+# class AccountsAdminSite(AdminSite):
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'last_name', 'first_name', 'email', 'username')
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('last_name', 'first_name', 'email'),
+        }),
+
+        ('Account Information', {
+            'fields': ('username', 'sub'),
+        }),
+
+        ('Integrations', {
+            'fields': ('civicrm_identifier', 'member_identifier')
+        }),
+
+        ('Advanced', {
+            'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')
+        })
+    )
