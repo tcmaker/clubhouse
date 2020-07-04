473ec5492dfae5b4024809333730ccbaaad346f6
Stephen Van Dahm <code@vandahm.com>
2020-07-04 15:02:11 -0500

    remove membership check

--- renew/views.py
+++ renew/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from .civicrm import renewal_url
 
 @login_required
-@membership_required
 def index(request):
     return render(request, 'renew/index.html', {
         'renewal_url': renewal_url(request.user),
