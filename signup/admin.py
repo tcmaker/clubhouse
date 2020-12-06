8cbc6ad4abe2432e3bbbbcea51b613833e1858c7
Stephen Van Dahm <code@vandahm.com>
2020-12-06 02:40:41 -0600

    admin panel update

--- signup/admin.py
+++ signup/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Registration
 
-# @admin.register(Registration)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('family_name', 'given_name', 'email', 'application_completed_at', 'keyfob_issued_at', 'account_invitation_created_at', 'account_invitation_accepted_at')
-#
-#     search_fields = ['family_name', 'given_name', 'email', 'civicrm_identifier', 'stripe_identifier']
-#
-#     ordering = ['family_name', 'given_name']
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('family_name', 'given_name', 'email', 'application_completed_at', 'keyfob_issued_at', 'account_invitation_created_at', 'account_invitation_accepted_at')
+
+    search_fields = ['family_name', 'given_name', 'email', 'civicrm_identifier', 'stripe_identifier']
+
+    ordering = ['family_name', 'given_name']
