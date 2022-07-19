342e54c5d0bfa77e709762aa9fa22e23fc6908dc
Joseph Bozarth <bozarjp@gmail.com>
2022-07-18 21:23:53 -0500

    issue-50: Removed links to signup fee step but left most of the code in pace incase we want it back

--- signup/urls.py
+++ signup/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', signup.index, name='index'),
     path('renew/', signup.renew, name='renew'),
     path('account/', signup.AccountView.as_view(), name=steps.ACCOUNT_INFO_STEP),
-    path('contact/', signup.ContactView.as_view(), name=steps.CONTACT_INFO_STEP),
-    path('setup-fee/', signup.SetupFeeView.as_view(), name=steps.PAYMENT_CARD_STEP),
+    path('contact/', signup.ContactView.as_view(), name=steps.CONTACT_INFO_STEP), 
+    #path('setup-fee/', signup.SetupFeeView.as_view(), name=steps.PAYMENT_CARD_STEP), 
     path('legal/', signup.LiabilityWaiverView.as_view(), name=steps.LEGAL_STEP),
     path('final/', signup.final, name=steps.DONE_STEP),
 ]
